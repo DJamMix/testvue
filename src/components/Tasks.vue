e0adade5051093b3e67a6ead965db4028ae4d7aa
DJamMix <antropov.viktor1234@yandex.ru>
2024-03-13 20:14:24 +0400

    Добавлена дата, статус задачи

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -10,17 +10,26 @@
         </v-col>
     </v-row>
     <v-container class="d-flex justify-center mb-6 flex-column">
-        <v-container>
-            <v-col md="8">
-                <v-textarea label="Заголовок задачи" rows="1"></v-textarea>
-            </v-col>
-            <v-col md="8">
-                <v-textarea label="Текст задачи"></v-textarea>
-            </v-col>
-        </v-container>
-        <v-container class="d-flex ml-4 justify-start">
-            <v-btn>Создать</v-btn>
-        </v-container>
+        <v-form fast-fail @submit.prevent>
+            <v-text-field
+                v-model="firstName"
+                label="Название задачи"
+            ></v-text-field>
+
+            <v-select
+                :items="statuses"
+                label="Выберите статус задачи"
+                required
+            >
+            </v-select>
+
+            <v-textarea
+                v-model="lastName"
+                label="Опишите задачу"
+            ></v-textarea>
+
+            <v-btn class="mt-2" type="submit" block>Создать задачу</v-btn>
+        </v-form>
     </v-container>
     <v-row class="list__cafes-title">
         <v-col>
@@ -31,31 +40,45 @@
         <v-row class="list__cafes-content">
             <v-col class="" md="12" v-for="task in tasks" :key="task.id">
                 <v-card>
+                    <div class="d-flex justify-space-between">
+                        <v-card-title>
+                            <h3 class="text-h5">{{ task.title }}</h3>
+                        </v-card-title>
+                        <v-card-title>
+                            <h3 class="text-h6">Статус: Новое</h3>
+                        </v-card-title>
+                    </div>
                     <v-card-title>
-                        <h3 class="text-h5">{{ task.title }}</h3>
+                        <h3 class="text-h6">Дата: 00.00.0000</h3>
                     </v-card-title>
                     <v-card-text>
                         <p class="text-body-1">{{ task.content }}</p>
                     </v-card-text>
                     <div class="pa-2 d-flex ga-2 justify-end">
-                        <v-btn @click="showEdit()">Редактировать</v-btn>
+                        <v-btn @click="showEdit(task.id)">Редактировать</v-btn>
                         <v-btn>Удалить</v-btn>
                     </div>
-                    <v-card v-if="showEditTask">
-                        <v-row class="list__cafes-title">
-                            <v-col>
-                                <h3 class="text-center text-h5 py-5">Редактирование задачи</h3>
-                            </v-col>
-                        </v-row>
-                        <v-col md="8">
-                            <input class="width100" type="text b-blue" v-model="task.title">
-                        </v-col>
-                        <v-col md="8">
-                            <textarea v-model="task.content" class="width100" rows="5"></textarea>
-                        </v-col>
-                        <div class="pa-2">
-                            <v-btn>Сохранить</v-btn> 
-                        </div>
+                    <v-card :id="'showEdit' + task.id" class="pa-5 d-none">
+                        <v-form fast-fail @submit.prevent>
+                            <v-text-field
+                                v-model="task.title"
+                                label="Изменить название задачи"
+                            ></v-text-field>
+
+                            <v-select
+                                :items="statuses"
+                                label="Изменить статус задачи"
+                                required
+                            >
+                            </v-select>
+
+                            <v-textarea
+                                v-model="task.content"
+                                label="Можете изменить содержимое задачи"
+                            ></v-textarea>
+
+                            <v-btn class="mt-2" type="submit" block>Сохранить</v-btn>
+                        </v-form>
                     </v-card>
                 </v-card>
             </v-col>
@@ -92,16 +115,24 @@ export default {
                 title: '',
                 content: '',
             },
-            showEditTask: false,
+            showEditTask: '',
+            statuses: [
+                'Новое',
+                'В работе',
+                'Отменено',
+                'Выполнено'
+            ],
         }
     },
     methods: {
-        showEdit()
+        showEdit(id)
         {
-            if(this.showEditTask == false) {
-                this.showEditTask = true
+            let idblock = 'showEdit' + id;
+            this.$el.setAttribute('id', idblock);
+            if(this.$el.classList.contains('d-none')) {
+                document.getElementById(idblock).classList.remove('d-none');
             } else {
-                this.showEditTask = false
+                document.getElementById(idblock).classList.add('d-none');
             }
         }
     }
